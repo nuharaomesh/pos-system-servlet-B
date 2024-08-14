bed57b14b70a7f244c7b09dba2afa0b96c701fa1
nuharaomesh <omeshnuhara4@gmail.com>
2024-08-14 17:42:29 +0530

    add CORS filter configuration

--- src/main/java/lk/ijse/possystemb/filter/CORSFilter.java
+++ src/main/java/lk/ijse/possystemb/filter/CORSFilter.java
@@ -1,7 +1,30 @@
 package lk.ijse.possystemb.filter;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+@WebFilter("/*")
 public class CORSFilter extends HttpFilter {
 
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        String origin = req.getHeader("Origin");
+        String config = getServletContext().getInitParameter("origin");
+
+        if (origin.contains(config)) {
+
+            res.setHeader("Access-Control-Allow-Origin", origin);
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTION");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            res.setHeader("Access-Control-Expose-Headers", "Content-Type");
+        }
+        chain.doFilter(req, res);
+    }
 }
