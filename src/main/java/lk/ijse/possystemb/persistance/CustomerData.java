cb14a7da054f0a0aac16cbc8bebefc5a210b1833
nuharaomesh <omeshnuhara4@gmail.com>
2024-08-22 18:58:22 +0530

    impl getAll customers process

--- src/main/java/lk/ijse/possystemb/persistance/CustomerData.java
+++ src/main/java/lk/ijse/possystemb/persistance/CustomerData.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface CustomerData {
 
-    List<CustomerDTO> getAll(Connection connection);
+    List<CustomerDTO> getAll(Connection connection) throws SQLException;
     boolean save(CustomerDTO dto, Connection connection) throws SQLException;
     boolean update(CustomerDTO dto, Connection connection);
     boolean delete(String id, Connection connection);
--- src/main/java/lk/ijse/possystemb/persistance/process/CustomerDataProcess.java
+++ src/main/java/lk/ijse/possystemb/persistance/process/CustomerDataProcess.java
@@ -4,16 +4,34 @@ import lk.ijse.possystemb.dto.CustomerDTO;
 import lk.ijse.possystemb.persistance.CustomerData;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDataProcess implements CustomerData {
 
     static String SAVE_CUSTOMER = "INSERT INTO Customer (id, name, gender, gmail, phoneNo) VALUES(?, ?, ?, ?, ?)";
+    static String GET_ALL_CUSTOMERS = "SELECT * FROM Csutomer"
 
     @Override
-    public List<CustomerDTO> getAll(Connection connection) {
-        return List.of();
+    public List<CustomerDTO> getAll(Connection connection) throws SQLException {
+
+        var customer= connection.prepareStatement(GET_ALL_CUSTOMERS);
+        ResultSet resultSet = customer.executeQuery();
+        List<CustomerDTO> customerList = new ArrayList<>();
+
+        while (resultSet.next()) {
+            CustomerDTO dto = new CustomerDTO();
+            dto.setId(resultSet.getString("id"));
+            dto.setName(resultSet.getString("id"));
+            dto.setGender(resultSet.getString("id"));
+            dto.setGmail(resultSet.getString("id"));
+            dto.setPhoneNo(resultSet.getInt("id"));
+        }
+
+        return customerList;
     }
 
     @Override
