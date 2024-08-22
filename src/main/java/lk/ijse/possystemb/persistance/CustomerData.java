189d2b62e84c821bc081d606e743535552e9e343
nuharaomesh <omeshnuhara4@gmail.com>
2024-08-22 18:44:14 +0530

    impl add customer process

--- src/main/java/lk/ijse/possystemb/persistance/CustomerData.java
+++ src/main/java/lk/ijse/possystemb/persistance/CustomerData.java
@@ -3,12 +3,13 @@ package lk.ijse.possystemb.persistance;
 import lk.ijse.possystemb.dto.CustomerDTO;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface CustomerData {
 
     List<CustomerDTO> getAll(Connection connection);
-    boolean save(CustomerDTO dto, Connection connection);
+    boolean save(CustomerDTO dto, Connection connection) throws SQLException;
     boolean update(CustomerDTO dto, Connection connection);
     boolean delete(String id, Connection connection);
 }
--- src/main/java/lk/ijse/possystemb/persistance/process/CustomerDataProcess.java
+++ src/main/java/lk/ijse/possystemb/persistance/process/CustomerDataProcess.java
@@ -4,9 +4,12 @@ import lk.ijse.possystemb.dto.CustomerDTO;
 import lk.ijse.possystemb.persistance.CustomerData;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CustomerDataProcess implements CustomerData {
+
+    static String SAVE_CUSTOMER = "INSERT INTO Customer (id, name, gender, gmail, phoneNo) VALUES(?, ?, ?, ?, ?)";
 
     @Override
     public List<CustomerDTO> getAll(Connection connection) {
@@ -14,8 +17,17 @@ public class CustomerDataProcess implements CustomerData {
     }
 
     @Override
-    public boolean save(CustomerDTO dto, Connection connection) {
-        return false;
+    public boolean save(CustomerDTO dto, Connection connection) throws SQLException {
+
+        var ps = connection.prepareStatement(SAVE_CUSTOMER);
+
+        ps.setString(1, dto.getId());
+        ps.setString(2, dto.getName());
+        ps.setString(3, dto.getGender());
+        ps.setString(4, dto.getGmail());
+        ps.setInt(5, dto.getPhoneNo());
+
+        return ps.executeUpdate() > 0;
     }
 
     @Override
