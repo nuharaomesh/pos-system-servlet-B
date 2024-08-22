7beac5feb90eca72ac9bf00704d1c44569cdd23c
nuharaomesh <omeshnuhara4@gmail.com>
2024-08-22 13:38:09 +0530

    update Customer process

--- src/main/java/lk/ijse/possystemb/persistance/CustomerData.java
+++ src/main/java/lk/ijse/possystemb/persistance/CustomerData.java
@@ -1,4 +1,14 @@
 package lk.ijse.possystemb.persistance;
 
+import lk.ijse.possystemb.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.util.List;
+
 public interface CustomerData {
+
+    List<CustomerDTO> getAll(Connection connection);
+    boolean save(CustomerDTO dto, Connection connection);
+    boolean update(CustomerDTO dto, Connection connection);
+    boolean delete(String id, Connection connection);
 }
--- src/main/java/lk/ijse/possystemb/persistance/process/CustomerDataProcess.java
+++ src/main/java/lk/ijse/possystemb/persistance/process/CustomerDataProcess.java
@@ -1,6 +1,30 @@
 package lk.ijse.possystemb.persistance.process;
 
+import lk.ijse.possystemb.dto.CustomerDTO;
 import lk.ijse.possystemb.persistance.CustomerData;
 
+import java.sql.Connection;
+import java.util.List;
+
 public class CustomerDataProcess implements CustomerData {
+
+    @Override
+    public List<CustomerDTO> getAll(Connection connection) {
+        return List.of();
+    }
+
+    @Override
+    public boolean save(CustomerDTO dto, Connection connection) {
+        return false;
+    }
+
+    @Override
+    public boolean update(CustomerDTO dto, Connection connection) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id, Connection connection) {
+        return false;
+    }
 }
