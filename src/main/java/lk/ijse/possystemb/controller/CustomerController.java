380d99e776c840382aa74b6ea46c6a3c72d2cdb2
nuharaomesh <omeshnuhara4@gmail.com>
2024-08-22 18:43:48 +0530

    impl add customer

--- src/main/java/lk/ijse/possystemb/controller/CustomerController.java
+++ src/main/java/lk/ijse/possystemb/controller/CustomerController.java
@@ -1,16 +1,19 @@
 package lk.ijse.possystemb.controller;
 
-import jakarta.servlet.ServletConfig;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.possystemb.dto.CustomerDTO;
 import lk.ijse.possystemb.persistance.CustomerData;
 import lk.ijse.possystemb.persistance.process.CustomerDataProcess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
@@ -21,26 +24,50 @@ public class CustomerController extends HttpServlet {
 
     private Connection connection;
     private CustomerData dataProcess = new CustomerDataProcess();
+    static Logger log = LoggerFactory.getLogger(CustomerController.class);
 
     @Override
     public void init() throws ServletException {
+
         try {
 
             var ctx = new InitialContext();
             DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/posSys");
             this.connection = pool.getConnection();
+            log.info("Connection Successfully created!");
         } catch (Exception e) {
+
+            log.error(e.getMessage());
             e.printStackTrace();
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
             resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
         }
 
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO dto = jsonb.fromJson(req.getReader(), CustomerDTO.class);
 
+        try (var writer = resp.getWriter()){
+
+            if (dataProcess.save(dto, connection)) {
+
+                log.info("Customer Successfully Saved!");
+                writer.write("Customer Saved!");
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            } else {
+                writer.write("Customer Not Saved!");
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        } catch (SQLException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Override
