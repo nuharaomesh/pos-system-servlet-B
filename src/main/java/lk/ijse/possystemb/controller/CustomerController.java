7e71f4bf1bcd15f380c254193b275c892954a3c6
nuharaomesh <omeshnuhara4@gmail.com>
2024-08-22 13:37:51 +0530

    create connection pool

--- src/main/java/lk/ijse/possystemb/controller/CustomerController.java
+++ src/main/java/lk/ijse/possystemb/controller/CustomerController.java
@@ -1,4 +1,60 @@
 package lk.ijse.possystemb.controller;
 
-public class CustomerController {
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.possystemb.persistance.CustomerData;
+import lk.ijse.possystemb.persistance.process.CustomerDataProcess;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/customer")
+public class CustomerController extends HttpServlet {
+
+    private Connection connection;
+    private CustomerData dataProcess = new CustomerDataProcess();
+
+    @Override
+    public void init() throws ServletException {
+        try {
+
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/posSys");
+            this.connection = pool.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
