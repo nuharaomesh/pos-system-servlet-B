b6ee688ee010b678ddf6af3d14068d1a5684e0dd
nuharaomesh <omeshnuhara4@gmail.com>
2024-08-29 14:06:06 +0530

    add custom entity

---
+++ src/main/java/lk/ijse/possystemb/entity/Custom.java
@@ -0,0 +1,13 @@
+package lk.ijse.possystemb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Custom {
+    private String id;
+    private int qty;
+}
