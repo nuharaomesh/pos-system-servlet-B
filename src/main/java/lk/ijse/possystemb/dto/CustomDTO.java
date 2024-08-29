eca6a1bec3ea325a6bec8f983e749b05bf604356
nuharaomesh <omeshnuhara4@gmail.com>
2024-08-29 11:22:54 +0530

    add custom dto

---
+++ src/main/java/lk/ijse/possystemb/dto/CustomDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.possystemb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomDTO {
+    private String id;
+    private int qty;
+}
